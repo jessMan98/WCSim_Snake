bbc64c2c842a71d194d0c580f3f9626730527551
Jesus <many_98@outlook.es>
2022-05-30 11:06:17 -0500

    Actualizando

--- WCSim/scripts/linux/creacionMAC.sh
+++ WCSim/scripts/linux/creacionMAC.sh
@@ -2,12 +2,12 @@
 
 # Para la ejecución del script, cambiar la ruta de la linea 5, 8, 9, 10 a la ruta de la máquina local.
 
-cd /home/many/Descargas/MCA_AutoSC-main/Crear_MAC
+cd ../../Crear_MAC
 
 echo "Eliminando archivos .mac anteriores..."
-rm -r /home/many/Descargas/MCA_AutoSC-main/data/1_MAC/VaryE/mu-/*.mac
-rm -r /home/autosc/Descargas/MCA_AutoSC-main/data/1_MAC/VaryE/e-/*.mac
-rm -r /home/autosc/Descargas/MCA_AutoSC-main/data/1_MAC/VaryE/gamma/*.mac
+rm -r ../../data/1_MAC/VaryE/mu-/*.mac
+rm -r ../../data/1_MAC/VaryE/e-/*.mac
+rm -r ../../data/1_MAC/VaryE/gamma/*.mac
 
 echo "Creando archivos .mac con particula mu-"
 python3 mac_files_config.py -i 12 --config_json config_mu.json -d ../data/1_MAC/VaryE/mu-
